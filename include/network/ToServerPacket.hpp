4873cb90269a84eb421935a244ec605a7c2c6df9
Cuba Giesbrecht <banishlight@gmail.com>
2024-09-04 10:51:43 -0600

    added server bound configuration state packet definitions

--- include/network/ToServerPacket.hpp
+++ include/network/ToServerPacket.hpp
@@ -16,3 +16,11 @@ void server_Login_Acknowledged(void);
 void server_Cookie_Reponse_login(int key, bool has_payload, VarInt payload_length, ByteArray payload);
 
 // Configuration State
+void server_Client_Information(string locale, Byte view_d, VarInt chat_mode, bool chat_colour, unsigned short display_skin_parts, VarInt main_hand, bool text_filter, bool server_listings);
+void server_Cookie_Response(Identifier key, bool has_payload, VarInt paylod_len, ByteArray payload);
+void server_Plugin_Message(Identifier channel, ByteArray data);
+void server_Acknowledge_Finish_Config(void);
+void server_Keep_Alive(Int64 id);
+void server_Pong(Int32 id);
+void server_Resource_Pack_Response(UUID uuid, VarInt result);
+void server_Known_Packs(VarInt pack_count, string[] name_space, string[] id);
