4ad907c622cc891638b724c46c1dc3d42573c5c5
Cuba Giesbrecht <banishlight@dillypickles.gateway.sasktel.net>
2024-10-03 15:15:17 +0000

    Fixed Socket.cpp format

--- src/network/Socket.cpp
+++ src/network/Socket.cpp
@@ -6,54 +6,41 @@
 Socket::Socket(Type type) : isBlocking(false),
 	status(DISCONNECT),
 	type(type),   
-    sockHandle(-1) 
-{
+    sockHandle(-1) {
 	
 }
 
-Socket::~Socket()
-{
+Socket::~Socket() {
 	Disconnect();
 }
-void Socket::SetBlocking(bool block)
-{
+
+void Socket::SetBlocking(bool block) {
 	//UInt64 mode = block ? 0 : 1; // Unused
 	Int32 opts = fcntl(sockHandle, F_GETFL);
 	if (opts < 0)return;
-	if(block)
-	{
+	if(block) {
 		opts |= O_NONBLOCK;
 	}
-	else
-	{
+	else {
 		opts &= ~O_NONBLOCK;
 	}
 	fcntl(sockHandle, F_SETFL, opts);
 	isBlocking = block;
 }
 
-bool Socket::Connect(const IPAddress& addr, UInt16 port)
-{
+bool Socket::Connect(const IPAddress& addr, UInt16 port) {
 	IPAddress address(addr);
 	return Connect(address, port);
 }
 
-size Socket::Send(DataBuffer& buffer)
-{
+size Socket::Send(DataBuffer& buffer) {
 	string data = buffer.ToString();
 	return this->Send(reinterpret_cast<const Byte*>(data.c_str()), data.length());
 }
 
-void Socket::Disconnect()
-{
-	if(sockHandle != -1)
-	{
+void Socket::Disconnect() {
+	if(sockHandle != -1) {
 		close(sockHandle);
 	}
 	status = DISCONNECT;
 }
-
-
-
-
-
