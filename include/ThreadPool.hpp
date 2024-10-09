c1e091a459a1cab09714c49b7e2191ae779fa356
Cuba Giesbrecht <banishlight@dillypickles.gateway.sasktel.net>
2024-10-09 16:26:38 +0000

    Further implementation of ThreadPool

--- include/ThreadPool.hpp
+++ include/ThreadPool.hpp
@@ -1,8 +1,20 @@
-
+#include <vector>
+#include <thread>
+#include <queue>
+#include <condition_variable>
+#include <mutex>
+#include <functional>
 
 class ThreadPool {
     public:
-
+        ThreadPool();
+        ~ThreadPool();
+        void enqueue(std::function<void()> task);
     private:
-        
+        std::vector<std::thread> workers;
+        std::queue<std::function<void()>> tasks;
+        std::mutex queueMutex;
+        std::condition_variable cv;
+
+        void workerThread();
 };
--- src/ThreadPool.cpp
+++ src/ThreadPool.cpp
@@ -1,3 +1,28 @@
 #include <ThreadPool>
+#include <thread>
 
+ThreadPool::ThreadPool(int num_threads) {
+    for(int i = 0; i < num_threads; i++) {
+        workers.emplace_back([this]() { workerThread(); });
+    }
+}
+
+ThreadPool::~ThreadPool() {
+    {
+        std::unique_lock<std::mutex> lock(queueMutex);
+        stop = true;
+    }
+    cv.notify_all();
+    for(std::thread& worker : workers) {
+        worker.join()
+    }
+}
+
+void enqueue(std::function<void()> task) {
+
+}
+
+void ThreadPool::workerThread() {
+
+}
 
