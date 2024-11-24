30edf33b3008e3dc20067e07c65f39abc41fec2a
Cuba Giesbrecht <banishlight@dillypickles.gateway.sasktel.net>
2024-11-24 02:13:09 +0000

    Changed ToServerPacket methods interface

--- src/network/ToServerPacket.cpp
+++ src/network/ToServerPacket.cpp
@@ -3,7 +3,8 @@
 #include <network/VarIntLong.hpp>
 
 // Handshake State
-void server_Handshake(VarInt protocol_version, string server_address, UInt16 server_port, VarInt next_state) {
+// VarInt protocol_version, string server_address, UInt16 server_port, VarInt next_state
+void server_Handshake(void* packet) {
 
 }
 
@@ -12,20 +13,24 @@ void server_Status_Request(void) {
 
 }
 
-void server_Ping_Request(Int64 payload) {
+// Int64 payload
+void server_Ping_Request(void* packet) {
 
 }
 
 // Login State
-void server_Login_Start(string name, UUID player_uuid) {
+// string name, UUID player_uuid
+void server_Login_Start(void* packet) {
 
 }
 
-void server_Encryption_Response(VarInt shared_secret_length, ByteArray shared_secret, VarInt verify_token_length, ByteArray verify_token) {
+// VarInt shared_secret_length, ByteArray shared_secret, VarInt verify_token_length, ByteArray verify_token
+void server_Encryption_Response(void* packet) {
 
 }
 
-void server_Login_Plugin_Response(VarInt message_id, bool success, ByteArray data) {
+// VarInt message_id, bool success, ByteArray data
+void server_Login_Plugin_Response(void* packet) {
 
 }
 
@@ -33,20 +38,24 @@ void server_Login_Acknowledged(void) {
 
 }
 
-void server_Cookie_Reponse_login(Int32 key, bool has_payload, VarInt payload_length, ByteArray payload) {
+// Int32 key, bool has_payload, VarInt payload_length, ByteArray payload
+void server_Cookie_Reponse_login(void* packet) {
 
 }
 
 // Configuration State
-void server_Client_Information(string locale, Byte view_d, VarInt chat_mode, bool chat_colour, Int16 display_skin_parts, VarInt main_hand, bool text_filter, bool server_listings) {
+// string locale, Byte view_d, VarInt chat_mode, bool chat_colour, Int16 display_skin_parts, VarInt main_hand, bool text_filter, bool server_listings
+void server_Client_Information(void* packet) {
 
 }
 
-void server_Cookie_Response(Identifier key, bool has_payload, VarInt paylod_len, ByteArray payload) {
+// Identifier key, bool has_payload, VarInt paylod_len, ByteArray payload
+void server_Cookie_Response(void* packet) {
 
 }
 
-void server_Plugin_Message(Identifier channel, ByteArray data) {
+// Identifier channel, ByteArray data
+void server_Plugin_Message(void* packet) {
 
 }
 
@@ -54,7 +63,8 @@ void server_Acknowledge_Finish_Config(void) {
 
 }
 
-void server_Keep_Alive(Int64 id) {
+// Int64 id
+void server_Keep_Alive(void* packet) {
 
 }
 
