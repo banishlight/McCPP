ed690563eff960d8d49aa4d3e52472c7ce879caa
Cuba Giesbrecht <banishlight@gmail.com>
2025-04-24 04:06:13 +0000

    Packet serializers finished

--- src/network/Packet.cpp
+++ src/network/Packet.cpp
@@ -19,3 +19,194 @@ int Pong_Response_p::serialize(void* out_buffer) const override {
     // TODO Implementation here
     return 0;
 }
+
+int Status_Request_p::deserialize(const void* in_buff) override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Ping_Request_status_p::deserialize(const void* in_buff) override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Disconnect_login_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Encryption_Response_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Login_Success_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Set_Compression_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Login_Plugin_Request_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Cookie_Request_login_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Login_Start_p::deserialize(const void* in_buff) override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Encryption_Response_p::deserialize(const void* in_buff) override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Login_Plugin_Response_p::deserialize(const void* in_buff) override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Login_Acknowledge_p::deserialize(const void* in_buff) override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Cookie_Response_login_p::deserialize(const void* in_buff) override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Cookie_Request_config_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Clientbound_Plugin_Message_config_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Disconnect_config_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Finish_Config_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Clientbound_Keep_Alive_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Ping_config_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Reset_Chat_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Registry_Data_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Remove_Resource_Pack_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Add_Resource_Pack_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Store_Cookie_config_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Transfer_config_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Feature_Flags_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Update_Tags_config_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Clientbound_Known_Packs_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Custom_Report_Details_config_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Server_Links_config_p::serialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Client_Information_config_p::deserialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Cookie_Response_config_p::deserialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Serverbound_Plugin_Message_config_p::deserialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Acknowledge_Finish_Config_p::deserialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Serverbound_Keep_Alive_config_p::deserialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Pong_config_p::deserialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Resource_Pack_Response_p::deserialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
+int Serverbound_Known_Packs_p::deserialize(void* out_buffer) const override {
+    // TODO Implementation here
+    return 0;
+}
+
